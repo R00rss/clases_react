2077dfefdaf047d5251bd50f5296bca1ddef59a7
Ronny Garcia <ronny.garcia@llacsaa.com>
2024-04-08 19:33:15 -0500

    init blank project react

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+const raiz_app_react = document.getElementById('root');
+
+ReactDOM.createRoot(raiz_app_react)
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
